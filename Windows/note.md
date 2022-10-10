e4a4e635f304c8b8fde17aa070f3d6723f89313a
Kopella <kopella.pvt@gmail.com>
2022-10-10 13:38:12 +0800

    Initialize project

---
+++ Windows/note.md
@@ -0,0 +1,87 @@
+# 【基础】OpenGL 学习笔记：OpenGL 的初始化方法 (Windows)
+
+在正式使用 OpenGL 之前， 我们必须先初始化它。OpenGL 为了保证平台无关性，并没有提供一个标准的初始化方法，每一个平台的处理方法都有一些不同。本篇笔记记录了 OpenGL 在 Windows 平台的初始化方法，并完成了一个简单的三角形 Demo。
+
+实际上相关的笔记在互联网上已经太多太多了，所以我尝试整了点不太一样的，即不引入第三方库，纯手动初始化 OpenGL。（不过其实这种笔记在互联网上也已经太多了）
+
+## 环境
+
+Windows 11, VS Code, CMake, Clang.
+
+## 流程
+
+OpenGL 的初始化流程可分为两部分。第一部分是为 OpenGL 创建上下文(Context)。第二部分是加载需要使用的 OpenGL 函数。
+
+### 为 OpenGL 创建上下文 (Context)
+
+在一般的 Demo 中， 这是像 GLFW 这样的库所完成的工作。OpenGL 的上下文为可以看作是一个容纳了所有 OpenGL 内容与功能的对象，它储存了 OpenGL 实例相关的所有状态。创建上下文的方法在不同的平台都有所不同。在 Windows 平台上我们依靠 Win32 API 中的初始化函数创建上下文，这些函数一般都有 wgl 作为前缀。
+
+在正式开始之前，我们首先需要创建一个窗口，并获取它的设备上下文。
+
+```cpp
+  HINSTANCE h_wgl_inst = GetModuleHandle(0);
+  HWND h_wgl_wnd = MyCreateWnd(h_wgl_inst);
+  HDC h_wgl_dc = GetDC(h_wgl_wnd);
+```
+
+函数 `HWND MyCreateWnd(HINSTANCE h_inst)` 是我自己定义的函数，窗口的具体创建过程请参考代码或是其他笔记。
+
+首先我们需要了解一种名为 Pixel Format 的对象，它描述了帧缓冲器所具有的属性。一般用户无法直接创建 Pixel Format， 我们需要填写一个用于描述它的结构体，将这个结构体交给函数 `ChoosePixelFormat` ，以此来获取设备上下文所支持的最接近的 Pixel Format，然后设置它。
+
+下面是一个示例，描述结构体的属性显然不会只有这么一点，更多的属性设置以及背后的意义可以参考[这里](https://learn.microsoft.com/en-us/windows/win32/api/wingdi/ns-wingdi-pixelformatdescriptor)。
+
+```cpp
+  PIXELFORMATDESCRIPTOR des_pf = {
+      .nSize = sizeof(des_pf),
+      .nVersion = 1,
+      .dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER,
+      .iPixelType = PFD_TYPE_RGBA,
+      .cColorBits = 32,
+      .cAlphaBits = 8,
+      .cDepthBits = 24,
+      .cStencilBits = 8,
+      .iLayerType = PFD_MAIN_PLANE,
+  };
+
+  int n_pf = ChoosePixelFormat(h_dc, &des_pf);
+  if (!n_pf) {
+    ErrorBox("Failed to find a suitable pixel format.");
+  }
+  if (!SetPixelFormat(h_dc, n_pf, &des_pf)) {
+    ErrorBox("Failed to set the pixel format.");
+  }
+```
+
+设置好 Pixel Format 后，创建上下文就很容易了。调用 `wglCreateContext`，它会返回一个上下文的句柄，将这个上下文设置为当前所使用的上下文后，就可以使用 OpenGL 了。
+
+```cpp
+  HGLRC h_rc = wglCreateContext(h_dc);
+  if (!h_rc) {
+    ErrorBox("Failed to create a OpenGL rendering context.");
+  }
+  if (!wglMakeCurrent(h_dc, h_rc)) {
+    ErrorBox("Failed to activate the OpenGL rendering context.");
+  }
+```
+
+OpenGL 的上下文是线程特定的，每一个线程都可以用以一个不同的上下文。需要注意的是，在多线程中使用相同的上下文是一件高风险的事情。
+
+### 为 OpenGL 加载函数
+
+在一般的 Demo 中， 这是像 GLAD 这样的库所完成的工作。OpenGL 不像其他的库只需要 include 进来就可以使用了。由于 OpenGL 本身实际上只是一套规范，具体的函数实现都是由平台和硬件方实现的，因此使用这些函数之前需要首先使用平台特定的 API 加载它们。
+
+加载函数的流程是这样子的：
+
+```cpp
+typedef GLvoid(APIENTRY* PFNGLBINDBUFFERPROC)(GLenum target, GLuint buffer);
+PFNGLBINDBUFFERPROC glBindBuffer =
+    (PFNGLBINDBUFFERPROC)wglGetProcAddress("glBindBuffer");
+```
+
+然后就可以正常使用函数 `glBindBuffer` 了。
+
+一般使用 OpenGL 的话，这一步骤还是交给第三方库来完成吧，手动载入函数并没有任何实质上的意义。在我的三角形 demo 中只是加载函数就占据了一半的代码量。
+
+## 代码
+
+[pure-ogl-triangle-demo]()
---
+++ Windows/win_main.cc
@@ -0,0 +1,420 @@
+#include "win_main.h"
+
+static GLvoid ErrorBox(LPCSTR msg) {
+  MessageBox(nullptr, msg, "Error", MB_OK | MB_ICONEXCLAMATION);
+  exit(EXIT_FAILURE);
+}
+
+int main() {
+  HINSTANCE h_wgl_inst = GetModuleHandle(0);
+  HWND h_wgl_wnd = MyCreateWnd(h_wgl_inst);
+  HDC h_wgl_dc = GetDC(h_wgl_wnd);
+
+  HGLRC h_wgl_rc = MyInitOgl(h_wgl_dc);
+
+  ShowWindow(h_wgl_wnd, SW_SHOW);
+  UpdateWindow(h_wgl_wnd);
+
+  GLuint shader_prog = MyOglLoadShader();
+  GLuint v_array_obj = MyOglLoadVertex();
+
+  bool running = true;
+  while (running) {
+    MSG msg;
+    while (PeekMessage(&msg, 0, 0, 0, PM_REMOVE)) {
+      if (msg.message == WM_QUIT) {
+        running = false;
+      } else {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+      }
+    }
+
+    glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    MyOglDrawScene(shader_prog, v_array_obj);
+
+    SwapBuffers(h_wgl_dc);
+  }
+
+  MyKillWgl(h_wgl_inst, h_wgl_wnd, h_wgl_dc, h_wgl_rc);
+  return 0;
+}
+
+HWND MyCreateWnd(HINSTANCE h_inst) {
+  WNDCLASS wnd_class = {
+      .style = CS_HREDRAW | CS_VREDRAW,
+      .lpfnWndProc = MyWndCallback,
+      .hInstance = h_inst,
+      .hCursor = LoadCursor(0, IDC_ARROW),
+      .hbrBackground = 0,
+      .lpszClassName = "Pure WGL Demo",
+  };
+
+  if (!RegisterClass(&wnd_class)) {
+    ErrorBox("Failed to register window class.");
+  }
+
+  RECT rect = {
+      .right = 960,
+      .bottom = 540,
+  };
+  AdjustWindowRect(&rect, WS_OVERLAPPEDWINDOW, false);
+
+  HWND h_wnd = CreateWindow(wnd_class.lpszClassName, "Pure WGL Demo",
+                            WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
+                            rect.right - rect.left, rect.bottom - rect.top, 0,
+                            0, h_inst, 0);
+
+  if (!h_wnd) {
+    ErrorBox("Failed to create window.");
+  }
+
+  return h_wnd;
+}
+
+LRESULT CALLBACK MyWndCallback(HWND h_wnd, UINT msg, WPARAM w_param,
+                               LPARAM l_param) {
+  LRESULT result = 0;
+
+  switch (msg) {
+    case WM_CLOSE:
+    case WM_DESTROY:
+      PostQuitMessage(0);
+      break;
+    case WM_SIZE:
+      MyReSizeGLScene(LOWORD(l_param), HIWORD(l_param));
+      break;
+    default:
+      result = DefWindowProcA(h_wnd, msg, w_param, l_param);
+      break;
+  }
+
+  return result;
+}
+
+PFNGLATTACHSHADERPROC glAttachShader;
+PFNGLBINDBUFFERPROC glBindBuffer;
+PFNGLBINDVERTEXARRAYPROC glBindVertexArray;
+PFNGLBUFFERDATAPROC glBufferData;
+PFNGLCOMPILESHADERPROC glCompileShader;
+PFNGLCREATEPROGRAMPROC glCreateProgram;
+PFNGLCREATESHADERPROC glCreateShader;
+PFNGLDELETEBUFFERSPROC glDeleteBuffers;
+PFNGLDELETEPROGRAMPROC glDeleteProgram;
+PFNGLDELETESHADERPROC glDeleteShader;
+PFNGLDELETEVERTEXARRAYSPROC glDeleteVertexArrays;
+PFNGLDETACHSHADERPROC glDetachShader;
+PFNGLENABLEVERTEXATTRIBARRAYPROC glEnableVertexAttribArray;
+PFNGLGENBUFFERSPROC glGenBuffers;
+PFNGLGENVERTEXARRAYSPROC glGenVertexArrays;
+PFNGLGETATTRIBLOCATIONPROC glGetAttribLocation;
+PFNGLGETPROGRAMINFOLOGPROC glGetProgramInfoLog;
+PFNGLGETPROGRAMIVPROC glGetProgramiv;
+PFNGLGETSHADERINFOLOGPROC glGetShaderInfoLog;
+PFNGLGETSHADERIVPROC glGetShaderiv;
+PFNGLLINKPROGRAMPROC glLinkProgram;
+PFNGLSHADERSOURCEPROC glShaderSource;
+PFNGLUSEPROGRAMPROC glUseProgram;
+PFNGLVERTEXATTRIBPOINTERPROC glVertexAttribPointer;
+PFNGLBINDATTRIBLOCATIONPROC glBindAttribLocation;
+PFNGLGETUNIFORMLOCATIONPROC glGetUniformLocation;
+PFNGLUNIFORMMATRIX4FVPROC glUniformMatrix4fv;
+PFNGLACTIVETEXTUREPROC glActiveTexture;
+PFNGLUNIFORM1IPROC glUniform1i;
+PFNGLGENERATEMIPMAPPROC glGenerateMipmap;
+PFNGLDISABLEVERTEXATTRIBARRAYPROC glDisableVertexAttribArray;
+PFNGLUNIFORM3FVPROC glUniform3fv;
+PFNGLUNIFORM4FVPROC glUniform4fv;
+
+BOOL MyLoadOglFunc(GLvoid) {
+  glAttachShader = (PFNGLATTACHSHADERPROC)wglGetProcAddress("glAttachShader");
+  if (!glAttachShader) {
+    return false;
+  }
+  glBindBuffer = (PFNGLBINDBUFFERPROC)wglGetProcAddress("glBindBuffer");
+  if (!glBindBuffer) {
+    return false;
+  }
+  glBindVertexArray =
+      (PFNGLBINDVERTEXARRAYPROC)wglGetProcAddress("glBindVertexArray");
+  if (!glBindVertexArray) {
+    return false;
+  }
+  glBufferData = (PFNGLBUFFERDATAPROC)wglGetProcAddress("glBufferData");
+  if (!glBufferData) {
+    return false;
+  }
+  glCompileShader =
+      (PFNGLCOMPILESHADERPROC)wglGetProcAddress("glCompileShader");
+  if (!glCompileShader) {
+    return false;
+  }
+  glCreateProgram =
+      (PFNGLCREATEPROGRAMPROC)wglGetProcAddress("glCreateProgram");
+  if (!glCreateProgram) {
+    return false;
+  }
+  glCreateShader = (PFNGLCREATESHADERPROC)wglGetProcAddress("glCreateShader");
+  if (!glCreateShader) {
+    return false;
+  }
+  glDeleteBuffers =
+      (PFNGLDELETEBUFFERSPROC)wglGetProcAddress("glDeleteBuffers");
+  if (!glDeleteBuffers) {
+    return false;
+  }
+  glDeleteProgram =
+      (PFNGLDELETEPROGRAMPROC)wglGetProcAddress("glDeleteProgram");
+  if (!glDeleteProgram) {
+    return false;
+  }
+  glDeleteShader = (PFNGLDELETESHADERPROC)wglGetProcAddress("glDeleteShader");
+  if (!glDeleteShader) {
+    return false;
+  }
+  glDeleteVertexArrays =
+      (PFNGLDELETEVERTEXARRAYSPROC)wglGetProcAddress("glDeleteVertexArrays");
+  if (!glDeleteVertexArrays) {
+    return false;
+  }
+  glDetachShader = (PFNGLDETACHSHADERPROC)wglGetProcAddress("glDetachShader");
+  if (!glDetachShader) {
+    return false;
+  }
+  glEnableVertexAttribArray =
+      (PFNGLENABLEVERTEXATTRIBARRAYPROC)wglGetProcAddress(
+          "glEnableVertexAttribArray");
+  if (!glEnableVertexAttribArray) {
+    return false;
+  }
+  glGenBuffers = (PFNGLGENBUFFERSPROC)wglGetProcAddress("glGenBuffers");
+  if (!glGenBuffers) {
+    return false;
+  }
+  glGenVertexArrays =
+      (PFNGLGENVERTEXARRAYSPROC)wglGetProcAddress("glGenVertexArrays");
+  if (!glGenVertexArrays) {
+    return false;
+  }
+  glGetAttribLocation =
+      (PFNGLGETATTRIBLOCATIONPROC)wglGetProcAddress("glGetAttribLocation");
+  if (!glGetAttribLocation) {
+    return false;
+  }
+  glGetProgramInfoLog =
+      (PFNGLGETPROGRAMINFOLOGPROC)wglGetProcAddress("glGetProgramInfoLog");
+  if (!glGetProgramInfoLog) {
+    return false;
+  }
+  glGetProgramiv = (PFNGLGETPROGRAMIVPROC)wglGetProcAddress("glGetProgramiv");
+  if (!glGetProgramiv) {
+    return false;
+  }
+  glGetShaderInfoLog =
+      (PFNGLGETSHADERINFOLOGPROC)wglGetProcAddress("glGetShaderInfoLog");
+  if (!glGetShaderInfoLog) {
+    return false;
+  }
+  glGetShaderiv = (PFNGLGETSHADERIVPROC)wglGetProcAddress("glGetShaderiv");
+  if (!glGetShaderiv) {
+    return false;
+  }
+  glLinkProgram = (PFNGLLINKPROGRAMPROC)wglGetProcAddress("glLinkProgram");
+  if (!glLinkProgram) {
+    return false;
+  }
+  glShaderSource = (PFNGLSHADERSOURCEPROC)wglGetProcAddress("glShaderSource");
+  if (!glShaderSource) {
+    return false;
+  }
+  glUseProgram = (PFNGLUSEPROGRAMPROC)wglGetProcAddress("glUseProgram");
+  if (!glUseProgram) {
+    return false;
+  }
+  glVertexAttribPointer =
+      (PFNGLVERTEXATTRIBPOINTERPROC)wglGetProcAddress("glVertexAttribPointer");
+  if (!glVertexAttribPointer) {
+    return false;
+  }
+  glBindAttribLocation =
+      (PFNGLBINDATTRIBLOCATIONPROC)wglGetProcAddress("glBindAttribLocation");
+  if (!glBindAttribLocation) {
+    return false;
+  }
+  glGetUniformLocation =
+      (PFNGLGETUNIFORMLOCATIONPROC)wglGetProcAddress("glGetUniformLocation");
+  if (!glGetUniformLocation) {
+    return false;
+  }
+  glUniformMatrix4fv =
+      (PFNGLUNIFORMMATRIX4FVPROC)wglGetProcAddress("glUniformMatrix4fv");
+  if (!glUniformMatrix4fv) {
+    return false;
+  }
+  glActiveTexture =
+      (PFNGLACTIVETEXTUREPROC)wglGetProcAddress("glActiveTexture");
+  if (!glActiveTexture) {
+    return false;
+  }
+  glUniform1i = (PFNGLUNIFORM1IPROC)wglGetProcAddress("glUniform1i");
+  if (!glUniform1i) {
+    return false;
+  }
+  glGenerateMipmap =
+      (PFNGLGENERATEMIPMAPPROC)wglGetProcAddress("glGenerateMipmap");
+  if (!glGenerateMipmap) {
+    return false;
+  }
+  glDisableVertexAttribArray =
+      (PFNGLDISABLEVERTEXATTRIBARRAYPROC)wglGetProcAddress(
+          "glDisableVertexAttribArray");
+  if (!glDisableVertexAttribArray) {
+    return false;
+  }
+  glUniform3fv = (PFNGLUNIFORM3FVPROC)wglGetProcAddress("glUniform3fv");
+  if (!glUniform3fv) {
+    return false;
+  }
+  glUniform4fv = (PFNGLUNIFORM4FVPROC)wglGetProcAddress("glUniform4fv");
+  if (!glUniform4fv) {
+    return false;
+  }
+  return true;
+}
+
+HGLRC MyInitOgl(HDC h_dc) {
+  PIXELFORMATDESCRIPTOR
+  des_pf = {
+      .nSize = sizeof(des_pf),
+      .nVersion = 1,
+      .dwFlags = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER,
+      .iPixelType = PFD_TYPE_RGBA,
+      .cColorBits = 32,
+      .cAlphaBits = 8,
+      .cDepthBits = 24,
+      .cStencilBits = 8,
+      .iLayerType = PFD_MAIN_PLANE,
+  };
+
+  int n_pf = ChoosePixelFormat(h_dc, &des_pf);
+  if (!n_pf) {
+    ErrorBox("Failed to find a suitable pixel format.");
+  }
+  if (!SetPixelFormat(h_dc, n_pf, &des_pf)) {
+    ErrorBox("Failed to set the pixel format.");
+  }
+  HGLRC h_rc = wglCreateContext(h_dc);
+  if (!h_rc) {
+    ErrorBox("Failed to create a OpenGL rendering context.");
+  }
+  if (!wglMakeCurrent(h_dc, h_rc)) {
+    ErrorBox("Failed to activate the OpenGL rendering context.");
+  }
+  if (!MyLoadOglFunc()) {
+    ErrorBox("Failed to load OpenGL's function.");
+  }
+
+  return h_rc;
+}
+
+GLvoid MyKillWgl(HINSTANCE h_inst, HWND h_wnd, HDC h_dc, HGLRC h_rc) {
+  if (!wglMakeCurrent(h_dc, 0)) {
+    ErrorBox("Failed to detach the rendering context.");
+  }
+  if (!wglDeleteContext(h_rc)) {
+    ErrorBox("Failed to delete the rendering context.");
+  }
+  if (!ReleaseDC(h_wnd, h_dc)) {
+    ErrorBox("Failed to release the device context.");
+  }
+  if (!DestroyWindow(h_wnd)) {
+    ErrorBox("Failed to destroy the window.");
+  }
+  if (!UnregisterClass("Pure WGL Demo", h_inst)) {
+    ErrorBox("Failed to unregister the class.");
+  }
+}
+
+GLvoid MyReSizeGLScene(GLsizei width, GLsizei height) {
+  glViewport(0, 0, width, height);
+}
+
+#define GL_ARRAY_BUFFER 0x8892
+#define GL_STATIC_DRAW 0x88E4
+#define GL_FRAGMENT_SHADER 0x8B30
+#define GL_VERTEX_SHADER 0x8B31
+#define GL_COMPILE_STATUS 0x8B81
+#define GL_LINK_STATUS 0x8B82
+#define GL_INFO_LOG_LENGTH 0x8B84
+#define GL_TEXTURE0 0x84C0
+#define GL_BGRA 0x80E1
+#define GL_ELEMENT_ARRAY_BUFFER 0x8893
+
+GLuint MyOglLoadShader() {
+  const char* v_shader_src =
+      "#version 330 core\n"
+      "layout (location = 0) in vec3 aPos;\n"
+      "void main()\n"
+      "{\n"
+      "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+      "}\0";
+  const char* f_shader_src =
+      "#version 330 core\n"
+      "out vec4 FragColor;\n"
+      "void main()\n"
+      "{\n"
+      "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+      "}\n\0";
+
+  GLuint v_shader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(v_shader, 1, &v_shader_src, nullptr);
+  glCompileShader(v_shader);
+
+  GLuint f_shader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(f_shader, 1, &f_shader_src, nullptr);
+  glCompileShader(f_shader);
+
+  GLuint shader_prog = glCreateProgram();
+  glAttachShader(shader_prog, v_shader);
+  glAttachShader(shader_prog, f_shader);
+  glLinkProgram(shader_prog);
+
+  glDeleteShader(v_shader);
+  glDeleteShader(f_shader);
+
+  return shader_prog;
+}
+
+GLuint MyOglLoadVertex() {
+  float vertices[] = {
+      -0.5f, -0.5f, 0.0f,  // left
+      0.5f,  -0.5f, 0.0f,  // right
+      0.0f,  0.5f,  0.0f   // top
+  };
+
+  GLuint v_buffer_obj, v_array_obj;
+  glGenVertexArrays(1, &v_array_obj);
+  glGenBuffers(1, &v_buffer_obj);
+
+  glBindVertexArray(v_array_obj);
+
+  glBindBuffer(GL_ARRAY_BUFFER, v_buffer_obj);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+  glBindVertexArray(0);
+
+  return v_array_obj;
+}
+
+GLvoid MyOglDrawScene(GLuint shader_prog, GLuint v_array_obj) {
+  glUseProgram(shader_prog);
+  glBindVertexArray(v_array_obj);
+  glDrawArrays(GL_TRIANGLES, 0, 3);
+}
